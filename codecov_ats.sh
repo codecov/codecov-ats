bbfce6fb6aeaea86538d443e63ace48b8b212a9f
Tom Hu <thomas.hu@sentry.io>
2023-11-03 14:03:00 +1100

    fix: double bracket

--- codecov_ats.sh
+++ codecov_ats.sh
@@ -6,7 +6,7 @@ echo $CODECOV_TOKEN
 
 pip install codecov-cli
 
-token=$( [ -n $INPUTS_CODECOV_TOKEN ] && echo $INPUTS_CODECOV_TOKEN || echo $CODECOV_TOKEN )
+token=$( [[ -n $INPUTS_CODECOV_TOKEN ]] && echo $INPUTS_CODECOV_TOKEN || echo $CODECOV_TOKEN )
 codecovcli create-commit -t ${token} || >&2 echo 'Codecov: Failed to properly create commit'
 codecovcli create-report -t ${token} || >&2 echo 'Codecov: Failed to properly create report'
 
