de21f870b7c7169e7882a32f4ec518afc2f3a56f
Tom Hu <thomas.hu@sentry.io>
2023-11-03 14:00:07 +1100

    fix: try the if

--- codecov_ats.sh
+++ codecov_ats.sh
@@ -6,8 +6,12 @@ echo $CODECOV_TOKEN
 
 pip install codecov-cli
 
-TOKEN=${$INPUTS_CODECOV_TOKEN || $CODECOV_TOKEN}
-codecovcli create-commit -t ${TOKEN} || >&2 echo 'Codecov: Failed to properly create commit'
-codecovcli create-report -t ${TOKEN} || >&2 echo 'Codecov: Failed to properly create report'
+if [ -z $INPUTS_CODECOV_TOKEN ]; then
+  token=$CODECOV_TOKEN
+else
+  token=$INPUTS_CODECOV_TOKEN
+fi
+codecovcli create-commit -t ${token} || >&2 echo 'Codecov: Failed to properly create commit'
+codecovcli create-report -t ${token} || >&2 echo 'Codecov: Failed to properly create report'
 
 # codecovcli static-analysis --token=${STATIC_TOKEN} || >&2 echo 'Codecov: Failed to properly execute static analysis'
