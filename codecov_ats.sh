749c0c249924eeb10679e5754de605050ed6b3de
Tom Hu <thomas.hu@sentry.io>
2023-11-03 13:34:50 +1100

    fix: push to INPUTS

--- codecov_ats.sh
+++ codecov_ats.sh
@@ -6,7 +6,8 @@ echo $CODECOV_TOKEN
 
 pip install codecov-cli
 
-codecovcli create-commit -t ${CODECOV_TOKEN} || >&2 echo 'Codecov: Failed to properly create commit'
-codecovcli create-report -t ${CODECOV_TOKEN} || >&2 echo 'Codecov: Failed to properly create report'
+TOKEN=${$INPUTS_CODECOV_TOKEN || $CODECOV_TOKEN}
+codecovcli create-commit -t ${TOKEN} || >&2 echo 'Codecov: Failed to properly create commit'
+codecovcli create-report -t ${TOKEN} || >&2 echo 'Codecov: Failed to properly create report'
 
-# codecovcli static-analysis --token=${CODECOV_STATIC_TOKEN} || >&2 echo 'Codecov: Failed to properly execute static analysis'
+# codecovcli static-analysis --token=${STATIC_TOKEN} || >&2 echo 'Codecov: Failed to properly execute static analysis'
